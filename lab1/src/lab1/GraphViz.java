bf7e487bcf3913454af3b05a8ffbb47c2d90e866
Gideon <1360502952@qq.com>
2017-10-06 14:17:09 +0800

    modify 3 files

--- lab1/src/lab1/GraphViz.java
+++ lab1/src/lab1/GraphViz.java
@@ -1,5 +1,5 @@
 package lab1;
-
+//No.1
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
--- lab1/src/lab1/Proba.java
+++ lab1/src/lab1/Proba.java
@@ -1,5 +1,5 @@
 package lab1;
-
+//No.2
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map.Entry;
